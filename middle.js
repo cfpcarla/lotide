e74f7672d48cfae82cd55ba474dcea7c8a7feeae
Carla <cfpcarla@gmail.com>
2019-12-02 18:41:13 +0000

    implement test inside of middle

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`🥳 Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function(list) {
   if (list.length < 3) {
@@ -34,9 +15,7 @@ const middle = function(list) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+module.exports = middle;
+
+
+
