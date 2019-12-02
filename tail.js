8d81bbdbc7b4ea826bd12b4ac68993593c28cb21
Carla <cfpcarla@gmail.com>
2019-12-02 17:04:36 +0000

    implement test inside of the tail

--- tail.js
+++ tail.js
@@ -1,24 +1,6 @@
+const assertEqual = require('./assertEqual');
 const tail = function(list) {
   return list.slice(1);
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥳 Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
-const result2 = tail([0]);
-assertEqual(result2.length, 0);
-const result3 = tail([]);
-assertEqual(result3.length, 0);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
