bf1e822385cf179718ea382ce98d47400d468d26
Carla <cfpcarla@gmail.com>
2019-12-02 16:59:24 +0000

    implement test inside of head

--- head.js
+++ head.js
@@ -1,17 +1,11 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(list) {
   return list[0];
+
+
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥳 Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = head;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([""]), "");
-assertEqual(head([]), undefined);
+
